60fed8c78be2c59f98741a5e3aeec6173ca753f1
IvanSelah <78192018+hahmsongmin@users.noreply.github.com>
2021-10-18 23:17:37 +0900

    라우터 설정

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import "./App.css";
+import AppRouter from "./Router";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return <AppRouter />;
 }
 
 export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,13 @@
 import firebase from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAlbmXjzsyY9CW36NmxFy9vidAEUeEA-wI",
-  authDomain: "twitter-24d84.firebaseapp.com",
-  projectId: "twitter-24d84",
-  storageBucket: "twitter-24d84.appspot.com",
-  messagingSenderId: "843522895838",
-  appId: "1:843522895838:web:90907cab5584c5e439774f",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);
